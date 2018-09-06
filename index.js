651f35576c6e7b335b1c0e596ea335feddd7947f
Saajid Khan <saajid.khan13@stjohns.edu>
2018-09-06 02:51:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// add solution here
+function theBeatlesPlay (players, instruments) {
+  var allPlayers= []; 
+    for (let i = 0; i < players.length; i++) {
+      allPlayers.push(players[i] + " plays " + instruments[i]) }
+      return allPlayers
+    }
+
+function johnLennonFacts(array) {
+  var newArray = [];
+  let i = 0;
+  while (i < array.length) {
+    newArray.push(`${array[i]}!!!`); i++
+  }
+  return newArray
+}
